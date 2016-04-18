8db0b331ac3b63a3d406725a2b4a15d87d878632
rugbyprof <terry.griffin@mwsu.edui>
2016-04-18 14:02:12 -0500

    adding concurrency code

---
+++ HelperFiles/producer_consumer_2.py
@@ -0,0 +1,40 @@
+from threading import Thread, Lock, Condition
+import time
+import random
+
+queue = []
+lock = Lock()
+
+condition = Condition()
+
+class ProducerThread(Thread):
+    def run(self):
+        nums = range(5)
+        global queue
+        while True:
+            condition.acquire()
+            num = random.choice(nums)
+            queue.append(num)
+            print("Produced", num)
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
+
+
+class ConsumerThread(Thread):
+    def run(self):
+        global queue
+        while True:
+            condition.acquire()
+            if not queue:
+                print("Nothing in queue, consumer is waiting")
+                condition.wait()
+                print("Producer added something to queue and notified the consumer")
+            num = queue.pop(0)
+            print("Consumed", num)
+            condition.release()
+            time.sleep(random.random())
+
+
+ProducerThread().start()
+ConsumerThread().start()
